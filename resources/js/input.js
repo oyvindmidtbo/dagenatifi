9fd06bfb693aeb27c61b22eb2baa52da10c38db4
Øyvind Midtbø <oyvind.midtbo@gmail.com>
2012-10-13 01:03:42 +0200

    Lagt til rødfarge på feil bokstaver

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,5 +1,6 @@
 var textToMatch = "Dette er teksten som skal matches.";
 var correctCharacters = 0;
+var redText = false;
 
 $(document).ready(function() {
 	$("#normalText").text(textToMatch);
@@ -11,19 +12,44 @@ $(document).ready(function() {
 function checkInputText(keyEvent) {
 	var keyPressed = String.fromCharCode(keyEvent.which);
 	var character = textToMatch.substring(correctCharacters, correctCharacters + 1);
+	
 	if (keyPressed == character) {
 		$("#greenText").text(function() {
 			var textToGreen = $("#greenText").text() + character;
 			return textToGreen;
 		});
 
-		$("#normalText").text(function() {
-			var normalText = $("#normalText").text();
-			var remainingNormalText = normalText.substring(1, normalText.length);
-			return remainingNormalText;
-		});
+		if (redText) {
+			$("#redText").text("");
+			redText = false;
+		} else {
+			$("#normalText").text(function() {
+				var normalText = $("#normalText").text();
+				var remainingNormalText = normalText.substring(1, normalText.length);
+				return remainingNormalText;
+			});	
+		}
+		
 
 		correctCharacters++;
+	} else {
+		if (!redText) {
+			$("#redText").text(function() {
+				var normalText = $("#normalText").text();
+				var textToRed = normalText.substring(0, 1);
+				return textToRed;
+			});
+
+			$("#normalText").text(function() {
+				var normalText = $("#normalText").text();
+				var remainingNormalText = normalText.substring(1, normalText.length);
+				return remainingNormalText;
+			});
+			
+			redText = true;
+		} else {
+
+		}
 	}
-	console.log("keypress: " + keyPressed);
+	console.log("keyPressed: " + keyPressed);
 }
