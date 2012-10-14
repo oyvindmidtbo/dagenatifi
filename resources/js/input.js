0f69b4d2a87cbe8a99f0dd479e17fcdf7e426972
Henrik Wingerei <henrikwingerei@gmail.com>
2012-10-14 19:48:20 +0200

    Refaktorert noe i data modulen.

--- resources/js/input.js
+++ resources/js/input.js
@@ -98,22 +98,28 @@ var game = (function() {
 	var data = (function() {
 		var textToMatch = "Dette er teksten som skal matches. Det ble stille og russeren viste hvordan den sorte ingeniøren fungerte. Plutselig så de tjueen hobitter som kom ved siden av kartongen. Boken kom og satt. En venn ristet ei løvinne bestemt. Soldaten lekte leiligheten rett ved en lysegrønn fiskebåt. En turist malte en osthøvel. Trompetene ankom, mens gullfiskene sang på turbinen. En figur malte en moped. En kjempehelt hørte datamaskinen til purkene.";
 		var textArray = textToMatch.split("");
-
-		return {
-			asArray: function() {
-				return textArray;
-			},
-			getValue: function(i) {
-				var text = textArray[i];
-				if(text === " ") {
-					return "&nbsp;"
-				} else {
-					return text;
-				}
-			},
-			length: function() {
-				return textArray.length;
+			
+		function asArray() {
+			return textArray;
+		}
+		
+		function getValue(i) {
+			var text = textArray[i];
+			if(text === " ") {
+				return "&nbsp;"
+			} else {
+				return text;
 			}
+		}
+		
+		function length() {
+			return textArray.length;
+		}
+		
+		return {
+			asArray: asArray,
+			getValue: getValue,
+			length: length
 		}
 	})();
 	
