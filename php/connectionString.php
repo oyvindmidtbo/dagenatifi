8f8a37fd2fdfa97f4ea78f9bfe6d92e8e0020c4e
Øyvind Midtbø <oyvind.midtbo@gmail.com>
2012-10-19 00:08:11 +0200

    Lagt til en fil for tilkobling til databasen

---
+++ php/connectionString.php
@@ -0,0 +1,40 @@
+<?php
+	class ConnectionString {
+   		public $host = '';
+   		public $username = '';
+   		public $password = '';
+   		public $database = '';
+
+    	public function getHost() {
+			if (parse_url($_SERVER['HTTP_HOST'], 1) != 'localhost') {
+				return $this->host;
+			} else {
+				return 'localhost';
+			}
+    	}
+
+    	public function getUsername() {
+			if (parse_url($_SERVER['HTTP_HOST'], 1) != 'localhost') {
+				return $this->username;
+			} else {
+				return 'root';
+			}
+    	}
+
+    	public function getPassword() {
+			if (parse_url($_SERVER['HTTP_HOST'], 1) != 'localhost') {
+        		return $this->password;
+			} else {
+				return 'root';
+			}
+    	}
+
+		public function getDatabase() {
+        	if (parse_url($_SERVER['HTTP_HOST'], 1) != 'localhost') {
+        		return $this->database;
+			} else {
+				return 'dagen';
+			}
+    	}
+	}
+?>
