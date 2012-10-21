cb1504960a5470bcb7d20a74ecf7eb4a8ea2b423
Øyvind Midtbø <oyvind.midtbo@gmail.com>
2012-10-22 01:00:29 +0200

    Bedre feilhåndtering dersom det blir feil fra php-kallet

--- js/io.js
+++ js/io.js
@@ -1,12 +1,15 @@
 var io = (function() {
-	function postScore(name, phone, mail, points) {
+	function postScore(name, phone, mail, points, successCallback, errorCallback) {
 		$.ajax({
 			type: "POST",
 			url: "php/io.php",
 			cache: false,
 			data:  {fn: "postScore", name: name, phone: phone, mail: mail, points: points},
 			success: function(data) {
-				// success code
+				successCallback(data);
+			},
+			error: function(data) {
+				errorCallback(data);
 			}
 		});
 	}
