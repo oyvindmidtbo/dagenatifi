05f5d0d26d0f800c5c63efd8416d3fdca0d0e18e
Henrik Wingerei <henrikwingerei@gmail.com>
2012-10-22 23:51:20 +0200

    Endret tilbake til POST, da GET ikke funker

--- js/io.js
+++ js/io.js
@@ -16,7 +16,7 @@ var io = (function() {
 
 	function getHighScoreList(callback) {
 		$.ajax({
-			type: "GET",
+			type: "POST",
 			url: "php/io.php",
 			cache: false,
 			data:  {fn: "getHighScoreList"},
