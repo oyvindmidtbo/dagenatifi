669e78f84c4d1c3999451837759f5ae01166bffd
Øyvind Midtbø <oyvind.midtbo@gmail.com>
2012-10-25 00:22:15 +0200

    La til en side der vinneren blir skrevet ut

--- js/score.js
+++ js/score.js
@@ -6,21 +6,27 @@ var score = (function() {
 			for (var i = 0; i < participants.length; i++) {
 				$("#highScoreList").append("<tr><td>" + (i + 1) + "</td><td>" + participants[i].name + "</td><td>" + participants[i].points + "</td></tr>");
 			}
-
-			console.log("Vinneren: " + participants[0].name);
-			console.log("Telefon: " + participants[0].phone);
-			console.log("E-postadresse: " + participants[0].mail);
-			console.log("Poeng: " + participants[0].mail);
-			console.log("******************************");
 		} else {
 			$("#highScoreList").html("<tr><th>Ingen deltagere</th></tr>");
 		}
+	}
 
+	function showWinner(participants) {
+		if (participants) {
+			$("#winner").html("<h1>Vinneren er:</h1>");
+			$("#winner").append("Navn: " + participants[0].name);
+			$("#winner").append("<br/>Telefon: " + participants[0].phone);
+			$("#winner").append("<br/>E-postadresse: " + participants[0].mail);
+			$("#winner").append("<br/>Poeng: " + participants[0].points);	
+		} else {
+			$("#winner").html("<h1>Ingen deltagere</h1>");
+		}
 		
 	}
 
 	return {
-		showHighScore: showHighScore
+		showHighScore: showHighScore,
+		showWinner: showWinner
 	}
 })();
 
@@ -28,6 +34,10 @@ $(function() {
 	setInterval(function() {
 		io.getHighScoreList(function(data) {
 			score.showHighScore(data);
-		});		
+		});
 	}, 2000);
+
+	io.getHighScoreList(function(data) {
+		score.showWinner(data);
+	});
 });
