652d2001c76b5d7144b892d3466f56c83fb46eab
Øyvind Midtbø <oyvind.midtbo@gmail.com>
2012-10-24 23:46:36 +0200

    Dersom det ikke er noen deltagere skrives dette ut

--- js/score.js
+++ js/score.js
@@ -1,16 +1,22 @@
 var score = (function() {
 	function showHighScore(participants) {
-		$("#highScoreList").html("<tr><th>Plass</th><th>Navn</th><th>Poeng</th></tr>");
+		if (participants) {
+			$("#highScoreList").html("<tr><th>Plass</th><th>Navn</th><th>Poeng</th></tr>");
 					
-		for (var i = 0; i < participants.length; i++) {
-			$("#highScoreList").append("<tr><td>" + (i + 1) + "</td><td>" + participants[i].name + "</td><td>" + participants[i].points + "</td></tr>");
+			for (var i = 0; i < participants.length; i++) {
+				$("#highScoreList").append("<tr><td>" + (i + 1) + "</td><td>" + participants[i].name + "</td><td>" + participants[i].points + "</td></tr>");
+			}
+
+			console.log("Vinneren: " + participants[0].name);
+			console.log("Telefon: " + participants[0].phone);
+			console.log("E-postadresse: " + participants[0].mail);
+			console.log("Poeng: " + participants[0].mail);
+			console.log("******************************");
+		} else {
+			$("#highScoreList").html("<tr><th>Ingen deltagere</th></tr>");
 		}
 
-		console.log("Vinneren: " + participants[0].name);
-		console.log("Telefon: " + participants[0].phone);
-		console.log("E-postadresse: " + participants[0].mail);
-		console.log("Poeng: " + participants[0].mail);
-		console.log("******************************");
+		
 	}
 
 	return {
@@ -23,5 +29,5 @@ $(function() {
 		io.getHighScoreList(function(data) {
 			score.showHighScore(data);
 		});		
-	}, 1000);
+	}, 2000);
 });
