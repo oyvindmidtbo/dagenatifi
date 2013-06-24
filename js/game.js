173d8ff0494627019d520b586d18437c8ebe5af5
Henrik Wingerei <henrikwingerei@gmail.com>
2013-06-24 22:31:08 +0200

    Noen refaktoreringer (mest flytting av kode/markup)

--- js/game.js
+++ js/game.js
@@ -41,7 +41,7 @@ var game = (function() {
 		}
 
 		if (character === " ") { 
-			var move = (span.position().left - prevMargin)
+			var move = span.position().left - prevMargin;
 			prevMargin = span.position().left;
 			$(".text").animate({
 				marginLeft: "-" + move + "px"
@@ -110,87 +110,3 @@ var game = (function() {
 		closeContactForm: closeContactForm
 	}
 })();
-
-$(function() {
-	// window.onbeforeunload = function(e) {
-	// 	e.preventDefault(); // prevent the user from leaving the page
-	// }
-
-	window.onkeydown = function (event) {
-	
-	if (!event) { /* This will happen in IE */
-		event = window.event;
-	}
-		
-	var keyCode = event.keyCode;
-	
-	if (keyCode == 8 &&
-		((event.target || event.srcElement).tagName != "TEXTAREA") && 
-		((event.target || event.srcElement).tagName != "INPUT")) { 
-		
-		if (navigator.userAgent.toLowerCase().indexOf("msie") == -1) {
-			event.stopPropagation();
-		} else {
-			alert("prevented");
-			event.returnValue = false;
-		}
-		
-		return false;
-	}
-};	
-
-	for(var i in data.asArray()) {
-		var value = data.getValue(i);
-		$(".text").append('<span id="' + i + '">' + value + '</span>');
-	}
-
-	$(document).keypress(function(event) {
-  		if(event.which === 13) {
-  			$(document).unbind("keypress");
-  			$(".start-button").click();
-  		}
-	});
-	
-	$(".start-button").click(function() {
-		$(".fancybox-wrap.start-wrap").hide(0, function() {
-			game.start();
-		});
-	})
-	
-	$(".final-result-close").click(function() {
-		game.closeContactForm();
-	})
-
-	$(".final-result").keypress(function(event) {
-		$(".status-message").slideUp(300);
-		var enterKeyCode = 13;
-		
-		if (event.which === enterKeyCode) {
-			var name = $("#participantName").val();
-			var phone = $("#participantPhone").val();
-			var mail = $("#participantMail").val();
-			var points = game.getScore();
-
-			if (name && phone && mail) {
-				io.postScore(name, phone, mail, points, function(data) {
-					$(".status-message").removeClass("status-message-error");
-					$(".status-message").text("Informasjonen ble lagret.");
-					$(".status-message").addClass("status-message-success");
-					$(".status-message").slideDown(300).delay(1000).slideUp(300, function() {
-						game.closeContactForm();
-					});
-				}, function(data) {
-					console.log("Feil under lagring av informasjon: ");
-					console.log(data);
-					$(".status-message").text("Feil under lagring av informasjonen.");
-					$(".status-message").addClass("status-message-error");
-					$(".status-message").slideDown(300);
-				});
-			} else {
-				$(".status-message").text("Ops, skriv inn all informasjon!");
-				$(".status-message").addClass("status-message-error");
-				$(".status-message").slideDown(300);
-			}
-		}
-	});
-});
--- js/timer.js
+++ js/timer.js
@@ -1,16 +1,16 @@
 var timer = (function() {
-	var seconds = 600; // ganger 10
-	var secondsToStart = 3;
+	var seconds = 6; // ganger 10
+	var secondsToStart = 0;
 	
 	function countdown(callback) {
 		$(".fancybox-wrap.countdown-wrap").show();
 		var countdownToStart = setInterval(function() {
 			if(secondsToStart === 0) {
-							clearInterval(countdownToStart);
-							$(".countdown").hide();
-							$("#fancybox-overlay").hide();
-							callback();
-						} else {
+				clearInterval(countdownToStart);
+				$(".countdown").hide();
+				$("#fancybox-overlay").hide();
+				callback();
+			} else {
 				$(".value").text(secondsToStart);
 				$(".countdown").show();
 				secondsToStart--;
@@ -21,7 +21,6 @@ var timer = (function() {
 	function start(callback) {
 		var countdown = setInterval(function() {
 			$("#secondsLeft").text(function() {
-				console.log(seconds);
 				if (seconds % 10 === 0) {
 					return seconds / 10 + ",0";
 				} else {
